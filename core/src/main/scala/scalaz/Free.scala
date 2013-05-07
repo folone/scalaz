5e9b07ca821ea839901bd41f5d78ac009ce99d27
folone <folone@gmail.com>
2013-05-07 11:55:58 +0200

    Instances and functions.

--- core/src/main/scala/scalaz/Free.scala
+++ core/src/main/scala/scalaz/Free.scala
@@ -251,6 +251,7 @@ trait FreeFunctions {
 sealed abstract class FreeA[F[+_], +A](implicit F: Functor[F]) {
   import FreeA._
   import syntax.applicative._
+  import syntax.std.function2._
   def run[B, G[+_]: Applicative](u: F[B] ⇒ G[B]): G[A] =
     this match {
       case Pure(x)        ⇒ Applicative[G].pure(x)
@@ -261,19 +262,35 @@ sealed abstract class FreeA[F[+_], +A](implicit F: Functor[F]) {
       case Pure(a)        ⇒ Pure(a)
       case Ap(x: F[B], y) ⇒ Ap(f(x), y.hoistAp(f))
     }
-  def pure[S >: A](a: S): FreeA[F, S] = Pure[F, S](a)
-  def map[B](f: A ⇒ B): FreeA[F, B] = this match {
-    case Pure(x)           ⇒ Pure(f(x))
-    case Ap(fa: F[A], apf) ⇒ Ap(fa, apf map (f compose _))
-  }
-  /*def <*>[B](fa: FreeA[F, A ⇒ B]): FreeA[F, B] = this match {
-    case Pure(f)  ⇒ fa.map(fun ⇒ fun(f))
-    case Ap(x, y) ⇒ Ap(x, (fa <*> y).map(fun ⇒ fun.flip))
-  }*/
+  def map[B](f: A ⇒ B): FreeA[F, B] =
+    this match {
+      case Pure(x)           ⇒ Pure(f(x))
+      case Ap(fa: F[A], apf) ⇒ Ap(fa, apf map (f compose _))
+    }
+  def <*>[B](fa: FreeA[F, A ⇒ B]): FreeA[F, B] =
+    this match {
+      case Pure(f)        ⇒ fa.map(fun ⇒ fun(f))
+      // FIXME having problems implementing the following
+      //case Ap(x: F[A], y) ⇒ Ap(x, fa <*> y.map(_.flip))
+    }
 }
 
-object FreeA {
-  case class Pure[F[+_]: Functor, +A](a: A) extends FreeA[F, A]
+object FreeA extends FreeAInstances with FreeAFunctions {
+  case class Pure[F[+_]: Functor, +A](a: A)                            extends FreeA[F, A]
   case class Ap[F[+_]: Functor, A, +B](fa: F[A], apf: FreeA[F, A ⇒ B]) extends FreeA[F, B]
+}
+
+trait FreeAInstances {
+  implicit def freeApplicative[F[+_]: Functor]: Applicative[({type λ[α] = FreeA[F, α]})#λ] =
+    new Applicative[({type λ[α] = FreeA[F, α]})#λ] {
+      def point[A](a: ⇒ A) = FreeA.pure(a)
+      override def map[A, B](fa: FreeA[F, A])(f: A ⇒ B): FreeA[F, B] = fa map f
+      def ap[A, B](fa: ⇒ FreeA[F, A])(f: ⇒ FreeA[F, A ⇒ B]): FreeA[F, B] = fa <*> f
+    }
+}
+
+trait FreeAFunctions {
+  import FreeA._
   def liftFreeA[F[+_]: Functor, A](x: F[A]): FreeA[F, A] = Ap[F, A, A](x, Pure(identity _))
+  def pure[F[+_]: Functor, A](a: ⇒ A) = Pure[F, A](a)
 }
